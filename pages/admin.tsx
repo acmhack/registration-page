67ffd5ede382c672401b4bd674acf6b7ee1e05b1
Jason Xu <Xinwen31415@gmail.com>
2023-02-06 15:31:03 -0600

    added admin and team pages to test link style changes on matched route

---
+++ pages/admin.tsx
@@ -0,0 +1,12 @@
+import { Title } from '@mantine/core';
+import { NextPage } from 'next/types';
+
+const Admin: NextPage = () => {
+	return (
+		<div>
+			<Title>Admin Panel?</Title>
+		</div>
+	);
+};
+
+export default Admin;
---
+++ pages/team.tsx
@@ -0,0 +1,12 @@
+import { Title } from '@mantine/core';
+import { NextPage } from 'next/types';
+
+const Team: NextPage = () => {
+	return (
+		<div>
+			<Title>Team Management</Title>
+		</div>
+	);
+};
+
+export default Team;
