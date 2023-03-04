5ab440d81bae5f5413c682fa67df3db1dab548b9
EricDuong <ericlamduong@gmail.com>
2023-03-03 22:01:50 -0600

    created test data added lodash for sorting data functions added mantine datatable, solves all problems haha

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,9 +1,13 @@
 import { useUser } from '@auth0/nextjs-auth0/client';
-import { useState } from 'react';
-import { Title, Table, ScrollArea, UnstyledButton, Group, Text, TextInput, AppShell, Navbar } from '@mantine/core';
+import { useEffect, useState } from 'react';
+import { useDisclosure } from '@mantine/hooks';
+import { Title, Table, ScrollArea, UnstyledButton, Group, Text, TextInput, Box } from '@mantine/core';
 import { keys } from '@mantine/utils';
 import { NextPage } from 'next/types';
-import { useEffect } from 'react';
+import { DataTable, DataTableSortStatus } from 'mantine-datatable';
+import sortBy from 'lodash/sortBy';
+
+import companies from './emails.json';
 
 const endpoint = 'https://nfn8sjemsh.execute-api.us-east-2.amazonaws.com/development/'
 
@@ -25,63 +29,67 @@ interface RowData {
 	company: string;
   }
   
-  interface TableSortProps {
+interface TableSortProps {
 	data: RowData[];
-  }
+}
 
-  const testData1: RowData = { name: "blob", email: "email", company: "company"}
-  const testData2: RowData = { name: "python", email: "anEmail", company: "goooge"}
-  const testArray: TableSortProps = { data: [testData1, testData2] }
+const testData1: RowData = { name: "blob", email: "email", company: "company"}
+const testData2: RowData = { name: "python", email: "anEmail", company: "goooge"}
+const testArray2: TableSortProps = { data: [testData1, testData2] }
+
+const testaData1 = [ "blob", "email", "company"];
+const testaData2 = [ "python", "anEmail", "googe"]
+const testArray = [testaData1, testaData2, testaData1]
   
-  interface ThProps {
+interface ThProps {
 	children: React.ReactNode;
 	reversed: boolean;
 	sorted: boolean;
 	onSort(): void;
-  }
+}
   
-  function Th({ children, reversed, sorted, onSort }: ThProps) {
+function Th({ children, reversed, sorted, onSort }: ThProps) {
 	return (
-	  <th>
+		<th>
 		<UnstyledButton onClick={onSort}>
-		  <Group position="apart">
+			<Group position="apart">
 			<Text weight={500} size="sm">
-			  {children}
+				{children}
 			</Text>
-		  </Group>
+			</Group>
 		</UnstyledButton>
-	  </th>
+		</th>
 	);
-  }
+}
   
-  function filterData(data: RowData[], search: string) {
+function filterData(data: RowData[], search: string) {
 	const query = search.toLowerCase().trim();
 	return data.filter((item) =>
-	  keys(data[0]).some((key) => item[key].toLowerCase().includes(query))
+		keys(data[0]).some((key) => item[key].toLowerCase().includes(query))
 	);
-  }
+}
   
-  function sortData(
+function sortData(
 	data: RowData[],
 	payload: { sortBy: keyof RowData | null; reversed: boolean; search: string }
-  ) {
+	) {
 	const { sortBy } = payload;
-  
+
 	if (!sortBy) {
-	  return filterData(data, payload.search);
+		return filterData(data, payload.search);
 	}
-  
+
 	return filterData(
-	  [...data].sort((a, b) => {
+		[...data].sort((a, b) => {
 		if (payload.reversed) {
-		  return b[sortBy].localeCompare(a[sortBy]);
+			return b[sortBy].localeCompare(a[sortBy]);
 		}
-  
+
 		return a[sortBy].localeCompare(b[sortBy]);
-	  }),
-	  payload.search
+		}),
+		payload.search
 	);
-  }
+}
   
 function TableSort({ data }: TableSortProps) {
 	const [search, setSearch] = useState('');
@@ -102,27 +110,23 @@ function TableSort({ data }: TableSortProps) {
 	  setSortedData(sortData(data, { sortBy, reversed: reverseSortDirection, search: value }));
 	};
   
+	//change this to fit my sick and twisted machinations
 	const rows = sortedData.map((row) => (
-	  <tr key={row.name}>
-		<td>{row.name}</td>
-		<td>{row.email}</td>
-		<td>{row.company}</td>
-	  </tr>
+		<tr key={row.name}>
+			<td>{row.name}</td>
+			<td>{row.email}</td>
+			<td>{row.company}</td>
+		</tr>
 	));
   
-	//scuffed beyond compare
 	return (
-		<AppShell
-			navbar={<Navbar width={{ base: 300}} height={400} p="xs">
-				<TextInput
-		  placeholder="Search by any field"
-		  mb="md"
-		  value={search}
-		  onChange={handleSearchChange}
-		/>
-		</Navbar>}
-		>
-			<ScrollArea>
+		<ScrollArea>
+			<TextInput
+		  		placeholder="Search by any field"
+		  		mb="md"
+		  		value={search}
+		  		onChange={handleSearchChange}
+			/>
 		<Table
 		  horizontalSpacing="md"
 		  verticalSpacing="xs"
@@ -168,8 +172,6 @@ function TableSort({ data }: TableSortProps) {
 		  </tbody>
 		</Table>
 	  </ScrollArea>
-		</AppShell>
-	  
 	);
   }
 
@@ -181,12 +183,34 @@ const Admin: NextPage = () => {
 			console.log(user);
 		}
 	}, [user, isLoading]);
+	
+	const [sortStatus, setSortStatus] = useState<DataTableSortStatus>({ columnAccessor: 'name', direction: 'asc' });
+	const [records, setRecords] = useState(sortBy(companies, 'name'))
+
+	useEffect(() => {
+		const data = sortBy(companies, sortStatus.columnAccessor);
+		setRecords(sortStatus.direction === 'desc' ? data.reverse() : data);
+	}, [sortStatus]);
 
 	return (
 		<div>
 			<Title>Admin Panel</Title>			
-			{ TableSort(testArray) }
-			
+			{/* TableSort(testArray) */}
+			<Box sx={{ height: 500}}>
+			<DataTable
+				withBorder
+				withColumnBorders
+				records={records}
+				columns={[
+					{ accessor: 'name', width: '40%', sortable: true },
+					{ accessor: 'streetAddress', width: '60%' },
+					{ accessor: 'city', width: 160, sortable: true },
+					{ accessor: 'state', width: 80, sortable: true },
+				]}
+				sortStatus={sortStatus}
+				onSortStatusChange={setSortStatus}
+				/>
+			</Box>
 		</div>
 	);
 };
