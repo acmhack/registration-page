878c1d9dbece5a327a89f4fdd13db8cddd0ae147
Jason Xu <Xinwen31415@gmail.com>
2023-03-13 22:46:39 -0500

    restructured users api route to accept id parameter too

---
+++ pages/api/users/[id].ts
@@ -0,0 +1,25 @@
+import { getSession, withApiAuthRequired } from '@auth0/nextjs-auth0';
+import axios from 'axios';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default withApiAuthRequired(async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+	const session = await getSession(req, res);
+	const id = session!.user.sub;
+
+	switch (req.method) {
+		case 'GET': {
+			const user = (await axios.get<DBEntry>(`${process.env.API_URL}/${id}`)).data;
+
+			if (user.admin) {
+				const response = await axios.get(`${process.env.API_URL}/${req.query.id}`);
+
+				return res.status(200).json(response.data);
+			} else {
+				return res.status(403).send("In the privacy interests of our hackers, only admins are allowed to view other users' data");
+			}
+		}
+		default:
+			res.status(405).json('Invalid Method; use GET');
+	}
+});
+
